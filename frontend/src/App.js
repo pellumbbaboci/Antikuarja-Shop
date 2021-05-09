c5af5a3d05a608079b01f93b3c6f20c3c7094a75
Pellumb Baboci <pellumbbaboci@gmail.com>
2021-05-09 16:14:47 +0200

    Header, footer and adding React-Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div className="App">
-        <h1>Welcome to shop</h1>
-    </div>
+    <>
+      <Header />
+      <Container className='text-center py3'>
+        <main className='py-3'>
+          <h1>Welcome to shop</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
