9276086e604f15d3e3c5d666ff2b5b6a4fd4e44a
Pellumb Baboci <pellumbbaboci@gmail.com>
2021-08-04 02:43:43 +0200

    remove unused vars

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
