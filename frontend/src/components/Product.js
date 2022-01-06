f96caf20c9d19ff64a2b8078c466b2aa2474674b
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-01-06 14:25:48 +0100

    fixed images and made contain

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,7 +7,9 @@ const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top'></Card.Img>
+        <Card.Img src={product.image} variant='top' style={{
+          objectFit: 'contain'
+        }} width={200} height={400}></Card.Img>
       </Link>
 
       <Card.Body>
