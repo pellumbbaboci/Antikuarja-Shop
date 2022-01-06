7a3ae0f872be8bd5ee5493a2c50e219668b3ad12
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-01-07 00:48:39 +0100

    get user by id and update user endpoints

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -77,7 +77,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 })
 
 // @desc Update user profile
-// @route POST /api/users/profile
+// @route PUT /api/users/profile
 // @access private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
@@ -103,6 +103,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+
 /**
  * @desc Get all users
  * @route GET /api/users
@@ -133,4 +134,48 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
+/**
+ * @desc Get user by id
+ * @route GET /api/users/:id
+ * @access private admin
+ */
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('--password')
+  if (user) {
+    res.status(200)
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+/**
+ *  @desc Update user by id
+ *  @route PUT /api/users/:id
+ *  @access private admin
+ */
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser } from '../controllers/userControllers.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userControllers.js'
 import { adminProtect } from '../middleware/authMiddleware.js'
 import { protect } from '../middleware/authMiddleware.js '
 
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.route('/').post(registerUser).get(protect, adminProtect, getUsers)
 
-router.route('/:id').delete(protect, adminProtect, deleteUser)
+router.route('/:id').delete(protect, adminProtect, deleteUser).get(protect, adminProtect, getUserById).put(protect, adminProtect, updateUser)
 
 /**
  * @swagger
